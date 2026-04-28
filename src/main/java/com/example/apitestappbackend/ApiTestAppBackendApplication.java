f195feabf94664a417768b02e37825bff2e8df07
Lê Việt Tiến <levieitien5744@gmail.com>
2026-04-28 14:15:35 +0700

    Initial commit

---
+++ src/main/java/com/example/apitestappbackend/ApiTestAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitestappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apitestappbackend/ApiTestAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apitestappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
